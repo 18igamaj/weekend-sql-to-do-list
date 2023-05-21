344a902b13a7ee8a30e403ff6fda5176ec2dc8ce
Majid Igale <majidigale18@gmail.com>
2023-05-20 20:09:43 -0500

    added get function and resolved matter in post

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,19 +2,62 @@ console.log('js');
 
 $(document).ready(function (){
     console.log('Yay client works!')
-$('#add-btn').on('click', postTD)
-
-})
+    $('#add-btn').on('click', postTD)
+    $('#istToDo').on('click','.comp-btn', putTD)
+    getTD()
+});
 
 function postTD(){
     console.log('post works')
-    let tInputs ={
-        toDo: $('#list-in').val()
-    }
-    console.log(tInputs)
-
+    let toDo =  $('#list-in').val();
     
+    console.log(toDo)
+
+    $.ajax({
+        method: 'POST',
+        url: '/doList',
+        data: {
+            toDo
+        }
+    }).then(function(response){
+        console.log(response)
+        getTD();
+    }).catch(function(error){
+        console.log(error)
+    });
+
 }
 
+function getTD(){
+    console.log('in getTD');
+
+    $.ajax({
+        method: 'GET',
+        url: '/doList'
+    }).then(function (response){
+        console.log('success')
+        renderDom(response)
+    }).catch(function (error){
+        alert('Oh No, our get TODOs')
+        console.log('get request failed', error)
+    })
+}
+
+function renderDom(array){
+    $('#listToDo').empty()
+
+
+    for(let item of array){
+        $('#listToDo').append(`
+
+        <tr id="${item.id}"data-id=${item.id}>
+            <td>${item.todo}</td>
+            <td><button class= "comp-btn">Complete</button></td> 
+            <td><button class= "del-btn">Delete</button></td>
+        </tr>
+        
+        `)
+    }
+}
 
 
--- server/routes/do-list.router.js
+++ server/routes/do-list.router.js
@@ -5,10 +5,34 @@ const router = express.Router();
 const pool = require('../modules/pool')
 
 
+router.get('/', (req,res) => {
+    let queryText = `SELECT * FROM "our_toDos" 
+    ORDER BY "id" ASC;`;
+    pool.query(queryText).then((result) => {
+        res.send(result.rows);
+        console.log('sending over rows', result.rows);
+    }).catch((error) => {
+        console.log('request for tasks failed', error);
+        res.sendStatus(500);
+    });
+});
 
 
+router.post('/', (req,res) => {
+    const newToDo = req.body
+    console.log(newToDo)
+    const queryText = `
+    INSERT INTO "our_toDos" ("todo")
+    VALUES ($1);`
 
-
+    pool.query(queryText,[newToDo.toDo]).then(result => {
+        res.sendStatus(201)
+    }).catch(error => {
+        console.log('Query text:', queryText, 'Our error is:', error)
+        res.sendStatus(500)
+    })
+    
+});
 
 
 
